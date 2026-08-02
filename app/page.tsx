784d7f03dba32ffcf35a3decebccc18bfd7f5538
ata-toku-0403 <s6t9ykhfhb@privaterelay.appleid.com>
2026-08-03 00:41:00 +0900

    YAHOOAPIの実装test5

--- app/page.tsx
+++ app/page.tsx
@@ -252,7 +252,7 @@ const search = async () => {
          rel="noopener noreferrer"
          className="mt-4 inline-block rounded-lg bg-red-500 px-4 py-2 text-center text-white hover:bg-red-600"
          >
-         {cheapest.Item.shop}で見る
+         {item.Item.shop}で見る
         </a>
       </div>
      </li>
